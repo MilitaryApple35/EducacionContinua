8236d948980093c71e3437c1bc30261c452d2292
MilitaryApple35 <pabloademir05@gmail.com>
2023-06-07 13:20:26 -0700

    Cambios
    Cambios

--- EC/models.py
+++ EC/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
-class Course(models.Model):
+class Courses(models.Model):
     id= models.AutoField(primary_key=True)
     imagen = models.ImageField(upload_to='cajas/', blank=False)
     titulo = models.CharField(max_length=200, blank=False)
     descripcion = models.TextField(blank=False)
     
     def __str__(self):
-        return self.name
+        return self.titulo
