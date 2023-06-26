592068902b9d96fa1d45c6003ea1e3cce2171af3
MilitaryApple35 <pabloademir05@gmail.com>
2023-06-26 10:55:30 -0700

    Cambios en el html y agregado forms.py

--- EC/static/js/scriptre.js
+++ EC/static/js/scriptre.js
@@ -5,19 +5,43 @@ window.onload = function() {
     var procedenciaRadios = document.getElementsByName('procedencia');
     var labelMatricula = document.getElementById('labelMatricula');
     var labelInstitucion = document.getElementById('labelInstitucion');
+    var carreraInput = document.getElementById("Carrera");
+    var labelCarrera = document.getElementById('labelCarrera')
+    institucionInput.classList.add('hidden');
+    labelInstitucion.classList.add('hidden');
+    matriculaInput.classList.add('hidden');
+    labelMatricula.classList.add('hidden');
+    carreraInput.classList.add('hidden');
+    labelCarrera.classList.add('hidden');
+    institucionInput.classList.remove('required');
+    matriculaInput.classList.remove('required');
   
     // Función para mostrar u ocultar los campos según la selección del radio
     function mostrarCampos() {
       if (procedenciaRadios[0].checked || procedenciaRadios[1].checked) {
-        matriculaInput.classList.remove('hidden');
         institucionInput.classList.add('hidden');
         labelInstitucion.classList.add('hidden');
+        institucionInput.classList.remove('required');
+        carreraInput.classList.remove('hidden');
+        labelCarrera.classList.remove('hidden');
+
+
+        matriculaInput.classList.remove('hidden');
         labelMatricula.classList.remove('hidden');
+        matriculaInput.classList.add('required');
+
       } else {
         matriculaInput.classList.add('hidden');
         labelMatricula.classList.add('hidden');
+        matriculaInput.classList.remove('required');
+        carreraInput.classList.remove('hidden');
+        labelCarrera.classList.remove('hidden');
+
+
         institucionInput.classList.remove('hidden');
         labelInstitucion.classList.remove('hidden');
+        institucionInput.classList.add('required');
+
       }
     }
   
