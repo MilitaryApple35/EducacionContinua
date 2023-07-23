94fd20c53463bcaaa380b0fbbeb6ccd4c0c1b993
MilitaryApple35 <pabloademir05@gmail.com>
2023-07-22 21:28:45 -0700

    Agregado login y autenticacion

--- PaginaAdmin/urls.py
+++ PaginaAdmin/urls.py
@@ -1,10 +1,10 @@
+# urls.py
 from django.urls import path
-from django.conf.urls.static import static
-from django.conf import settings
-from PaginaAdmin import views
+from . import views
 
 urlpatterns = [
     # Otras URLs de tu aplicación
-    path('login/', views.login_view, name='login'),
+    path('', views.login_view, name='login'),
     path('administracion/', views.administracion, name='administracion'),
 ]
+
--- PaginaAdmin/views.py
+++ PaginaAdmin/views.py
@@ -1,10 +1,25 @@
 # views.py
-from django.contrib.auth.views import login
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from EC.models import Courses
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('administracion')
+        else:
+            # Si el usuario no existe o las credenciales son incorrectas, puedes mostrar un mensaje de error en la plantilla
+            error_message = "Usuario o contraseña incorrectos"
+            return render(request, 'login.html', {'error_message': error_message})
+    return render(request, 'login.html')
 
 @login_required
 def administracion(request):
     # Tu código de la vista de administración aquí
-    cursos = Curso.objects.all()
+    cursos = Courses.objects.all()
     return render(request, 'administracion.html', {'cursos': cursos})
