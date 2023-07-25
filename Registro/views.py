83e2dc163bfc61e97f42103016f2b58e7f5681bd
MilitaryApple35 <pabloademir05@gmail.com>
2023-07-25 07:30:00 -0700

    Agregado Requirements.txt

--- Registro/views.py
+++ Registro/views.py
@@ -10,7 +10,7 @@ def register(request):
         form = RegistroForm(request.POST)
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect("")
+            return HttpResponseRedirect("") 
     else:
         form = RegistroForm()
     
