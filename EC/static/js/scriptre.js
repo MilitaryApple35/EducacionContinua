3d30e1305dff4bedabe6abd3deb0da105b62bc95
MilitaryApple35 <pabloademir05@gmail.com>
2023-08-10 16:25:10 -0700

    Agregados selectores en el registro (WIP)
    Falta que se oculten

--- EC/static/js/scriptre.js
+++ EC/static/js/scriptre.js
@@ -3,10 +3,17 @@ window.onload = function() {
     var matriculaInput = document.getElementById('Matricula');
     var institucionInput = document.getElementById('Institucion');
     var procedenciaRadios = document.getElementsByName('procedencia');
+    var tipoRadios = document.getElementsByName('tipoRegistro')
     var labelMatricula = document.getElementById('labelMatricula');
     var labelInstitucion = document.getElementById('labelInstitucion');
     var carreraInput = document.getElementById("Carrera");
     var labelCarrera = document.getElementById('labelCarrera')
+    var selectorCursos = document.getElementById('selector-Cursos')
+    var selectorConferencias = document.getElementById('selector-Conferencias')
+    var selectorTalleres = document.getElementById('selector-Talleres')
+    var selectorDiplomados = document.getElementById('selector-Diplomados')
+    var selectorCongresos = document.getElementById('selector-Congresos')
+    var selectorCapacitaciones = document.getElementById('selector-Capacitaciones')
     institucionInput.classList.add('hidden');
     labelInstitucion.classList.add('hidden');
     matriculaInput.classList.add('hidden');
@@ -15,6 +22,12 @@ window.onload = function() {
     labelCarrera.classList.add('hidden');
     institucionInput.classList.remove('required');
     matriculaInput.classList.remove('required');
+    selectorCursos.classList.add('hidden');
+    selectorConferencias.classList.add('hidden');
+    selectorTalleres.classList.add('hidden');
+    selectorDiplomados.classList.add('hidden');
+    selectorCongresos.classList.add('hidden');
+    selectorCapacitaciones.classList.add('hidden');
   
     // Función para mostrar u ocultar los campos según la selección del radio
     function mostrarCampos() {
@@ -50,5 +63,22 @@ window.onload = function() {
     // Asignar evento onclick a los radios para llamar a la función mostrarCampos
     for (var i = 0; i < procedenciaRadios.length; i++) {
       procedenciaRadios[i].onclick = mostrarCampos;
+    }
+
+    function mostrarSelector(selectorId) {
+      var selectores = document.getElementsByClassName('selector-tipo');
+      
+      for (var i = 0; i < selectores.length; i++) {
+        selectores[i].classList.add('hidden');
+      }
+      
+      var selector = document.getElementById(selectorId);
+      if (selector) {
+        selector.classList.remove("hidden");
+      }
+    }
+
+    for (var i = 0; i < tipoRadios.length; i++) {
+      tipoRadios[i].onclick = mostrarSelector;
     }
 };
--- Registro/views.py
+++ Registro/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
-from EC.models import Courses
+from EC.models import Courses, Capacitaciones, Diplomados, Conferencias, Congresos, Talleres
 from .forms import RegistroForm
 from EC.views import main
 # Create your views here.
 
 def register(request):
     courses = Courses.objects.all()
+    capacitaciones= Capacitaciones.objects.all()
+    diplomados = Diplomados.objects.all()
+    congresos = Congresos.objects.all()
+    conferencias = Conferencias.objects.all()
+    talleres = Talleres.objects.all()
     if request.method == 'POST':
         form = RegistroForm(request.POST)
         if form.is_valid():
@@ -15,5 +20,4 @@ def register(request):
     else:
         form = RegistroForm()
     
-    return render(request, 'register.html', {'courses': courses, 'form': form, 'errors': form.errors})
-
+    return render(request, 'register.html', {'courses': courses, 'form': form, 'errors': form.errors , 'capacitaciones': capacitaciones,'diplomados': diplomados, 'congresos': congresos, 'conferencias': conferencias, 'talleres': talleres})
